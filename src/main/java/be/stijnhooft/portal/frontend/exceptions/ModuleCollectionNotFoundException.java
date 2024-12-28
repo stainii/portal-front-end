5623f7b8a5feb2e02cd43ad433e909bd5679d62c
stainii <stijnhooft@hotmail.com>
2024-12-28 20:31:23 +0100

    Upgrade to Java 17, with Openrewrite

--- src/main/java/be/stijnhooft/portal/frontend/exceptions/ModuleCollectionNotFoundException.java
+++ src/main/java/be/stijnhooft/portal/frontend/exceptions/ModuleCollectionNotFoundException.java
@@ -2,6 +2,6 @@ package be.stijnhooft.portal.frontend.exceptions;
 
 public class ModuleCollectionNotFoundException extends RuntimeException {
     public ModuleCollectionNotFoundException(String user) {
-        super(String.format("No module collection for user %s found", user));
+        super("No module collection for user %s found".formatted(user));
     }
 }
