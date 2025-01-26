20f5eb75c9e8f85912c0e30a43d9081811712db7
stainii <stijnhooft@hotmail.com>
2025-01-26 09:52:58 +0100

    Get rid of legacy Material components in the user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,12 +3,12 @@ import {CommonModule} from '@angular/common';
 import {LoginComponent} from "@app/user/login/login.component";
 import {LogoutComponent} from "@app/user/logout/logout.component";
 import {FormsModule} from "@angular/forms";
-import {MatLegacyInputModule as MatInputModule} from "@angular/material/legacy-input";
-import {MatLegacyFormFieldModule as MatFormFieldModule} from "@angular/material/legacy-form-field";
-import {MatLegacyCardModule as MatCardModule} from "@angular/material/legacy-card";
-import {MatLegacyListModule as MatListModule} from "@angular/material/legacy-list";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+import {MatListModule} from "@angular/material/list";
 import {MatIconModule} from "@angular/material/icon";
-import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
